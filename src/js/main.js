2685c1b641c69e3b0dc9d3b2bbc1621a57cf1712
Daniil_Khobotov <dinich44@gmail.com>
2023-08-31 18:15:01 +0400

    add pc version

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,33 @@
+import WOW from 'wow.js';
+import glideSliders from './modules/glideSliders';
+import highlightMenuObjects from './modules/highlightMenuObjects';
 
+window.addEventListener('DOMContentLoaded', () => {
 
-// window.addEventListener('DOMContentLoaded', () => {
+		//Menu: static/dynamic 
+		const headerBlock = document.getElementById('header');
 
+		window.onscroll = function () {
+			if (window.pageYOffset>90) {
+				headerBlock.style.backgroundColor = 'var(--header-bg)';
+			} else {
+				headerBlock.style.backgroundColor = 'rgba(0,0,0,0)';
+			}
+		};backgroundColor
 
-// });
+    //WoW.js
+	const wow = new WOW({
+		boxClass: 'wow',
+		animateClass: 'animated',
+		offset: 100,
+		mobile: true,  
+		live: true,
+	});
+	wow.init();
+
+	try {
+		glideSliders();
+	} catch(e) {}
+
+	highlightMenuObjects('.menu__item__link', 'menu__item__link_active');
+});
---
+++ src/js/modules/glideSliders.js
@@ -0,0 +1,59 @@
+import Glide from '@glidejs/glide';
+
+function glideSliders () {
+    const partnersSlider = new Glide('.partners', {
+        type: 'carousel',		
+        startAt: 0,
+        perView: 5,
+        gap: 10,
+        autoplay: 5000,
+        hoverpause: false,
+        focusAt: 'center',
+        animationTimingFunc: 'ease-in-out',
+        animationDuration: 1000,
+    });
+    partnersSlider.mount();
+
+
+    // const pricesSlider = new Glide('.prices', {
+    //     type: 'carousel',		
+    //     startAt: 0,
+    //     perView: 3,
+    //     gap: -30,
+    //     breakpoints: {
+    //         767: {
+    //             perView: 2,
+    //             gap: -70,
+    //         },
+    //         575: {
+    //             perView: 1,
+    //             gap: -250,
+    //         },
+    //         512: {
+    //             perView: 1,
+    //             gap: -180,
+    //         },
+    //         440: {
+    //             perView: 1,
+    //             gap: -100,
+    //         },
+    //         360: {
+    //             perView: 1,
+    //             gap: -70,
+    //         },
+    //         320: {
+    //             perView: 1,
+    //             gap: -50,
+    //         }
+    //     },
+    //     autoplay: 5000,
+    //     hoverpause: true,
+    //     focusAt: 'center',
+    //     animationTimingFunc: 'ease-in-out',
+    //     animationDuration: 500,
+    // });
+    // pricesSlider.mount();
+
+}
+
+export default glideSliders;

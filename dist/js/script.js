df9e5beb1ab1c0e816c3bee59b91e7a8668642db
Daniil_Khobotov <dinich44@gmail.com>
2023-09-01 15:26:10 +0400

    services adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -4576,6 +4576,23 @@ function glideSliders() {
     startAt: 0,
     perView: 5,
     gap: 10,
+    breakpoints: {
+      1199: {
+        gap: 5
+      },
+      991: {
+        perView: 3,
+        gap: 10
+      },
+      767: {
+        perView: 3,
+        gap: 5
+      },
+      575: {
+        perView: 1,
+        gap: -80
+      }
+    },
     autoplay: 5000,
     hoverpause: false,
     focusAt: 'center',

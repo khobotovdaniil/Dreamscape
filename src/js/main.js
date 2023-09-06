8b5f5ec271bcd29ac672aa030a20aaf55d1ed018
Daniil_Khobotov <dinich44@gmail.com>
2023-09-06 18:24:09 +0400

    fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -8,16 +8,16 @@ import checkboxValidation from './modules/checkboxValidation';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-		//Menu: static/dynamic 
-		const headerBlock = document.getElementById('header');
+	//Menu: static/dynamic 
+	const headerBlock = document.getElementById('header');
 
-		window.onscroll = function () {
-			if (window.pageYOffset>90) {
-				headerBlock.style.backgroundColor = 'var(--header-bg)';
-			} else {
-				headerBlock.style.backgroundColor = 'rgba(0,0,0,0)';
-			}
-		};
+	window.onscroll = function () {
+		if (window.pageYOffset>50) {
+			headerBlock.style.backgroundColor = 'var(--header-bg)';
+		} else {
+			headerBlock.style.backgroundColor = 'rgba(0,0,0,0)';
+		}
+	};
 
     //WoW.js
 	const wow = new WOW({
@@ -35,11 +35,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	highlightMenuObjects('.menu__item__link', 'menu__item__link_active');
 	
-	
-	changingNumbers('.number-1');
-	changingNumbers('.number-2');
-	changingNumbers('.number-3');
-	changingNumbers('.number-4');
+	try {
+		changingNumbers('.number-1');
+		changingNumbers('.number-2');
+		changingNumbers('.number-3');
+		changingNumbers('.number-4');
+	} catch(e) {}
 
 	modals();
 	checkboxValidation();
--- src/js/modules/glideSliders.js
+++ src/js/modules/glideSliders.js
@@ -32,46 +32,6 @@ function glideSliders () {
     });
     partnersSlider.mount();
 
-
-    // const pricesSlider = new Glide('.prices', {
-    //     type: 'carousel',		
-    //     startAt: 0,
-    //     perView: 3,
-    //     gap: -30,
-    //     breakpoints: {
-    //         767: {
-    //             perView: 2,
-    //             gap: -70,
-    //         },
-    //         575: {
-    //             perView: 1,
-    //             gap: -250,
-    //         },
-    //         512: {
-    //             perView: 1,
-    //             gap: -180,
-    //         },
-    //         440: {
-    //             perView: 1,
-    //             gap: -100,
-    //         },
-    //         360: {
-    //             perView: 1,
-    //             gap: -70,
-    //         },
-    //         320: {
-    //             perView: 1,
-    //             gap: -50,
-    //         }
-    //     },
-    //     autoplay: 5000,
-    //     hoverpause: true,
-    //     focusAt: 'center',
-    //     animationTimingFunc: 'ease-in-out',
-    //     animationDuration: 500,
-    // });
-    // pricesSlider.mount();
-
 }
 
 export default glideSliders;
